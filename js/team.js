9bb4117c664275374af0980c1dfa342cce0cc7e4
葛俊卿 <gejunqing@gejunqingdeMacBook-Pro.local>
2016-06-23 10:32:02 +0800

    abc 2222222

---
+++ js/team.js
@@ -0,0 +1,31 @@
+/**
+ * Created by gejunqing on 16/6/21.
+ */
+var ETEAM = {
+    France           :{cname:'法国',littleimg:'France.png',bigimg:'France-b.png',ename:'France'},
+    Germany          :{cname:'德国',littleimg:'Germany.png',bigimg:'Germany-b.png',ename:'Germany'},
+    England          :{cname:'英格兰',littleimg:'England.png',bigimg:'England-b.png',ename:'England'},
+    Portugal         :{cname:'葡萄牙',littleimg:'Portugal.png',bigimg:'Portugal-b.png',ename:'Portugal'},
+    Belgium          :{cname:'比利时',littleimg:'Belgium.png',bigimg:'Belgium-b.png',ename:'Belgium'},
+    Italy            :{cname:'意大利',littleimg:'Italy.png',bigimg:'Italy-b.png',ename:'Italy'},
+    Russia           :{cname:'俄罗斯',littleimg:'Russia.png',bigimg:'Russia-b.png',ename:'Russia'},
+    Switzerland      :{cname:'瑞士',littleimg:'Switzerland.png',bigimg:'Switzerland-b.png',ename:'Switzerland'},
+    Austria          :{cname:'奥地利',littleimg:'Austria.png',bigimg:'Austria-b.png',ename:'Austria'},
+    Croatia          :{cname:'克罗地亚',littleimg:'Croatia.png',bigimg:'Croatia-b.png',ename:'Croatia'},
+    Ukraine          :{cname:'乌克兰',littleimg:'Ukraine.png',bigimg:'Ukraine-b.png',ename:'Ukraine'},
+    Sweden           :{cname:'瑞典',littleimg:'Sweden.png',bigimg:'Sweden-b.png',ename:'Sweden'},
+    Poland           :{cname:'波兰',littleimg:'Poland.png',bigimg:'Poland-b.png',ename:'Poland'},
+    Romania          :{cname:'罗马尼亚',littleimg:'Romania.png',bigimg:'Romania-b.png',ename:'Romania'},
+    Slovakia         :{cname:'斯洛伐克',littleimg:'Slovakia.png',bigimg:'Slovakia-b.png',ename:'Slovakia'},
+    Hungary          :{cname:'匈牙利',littleimg:'Hungary.png',bigimg:'Hungary-b.png',ename:'Hungary'},
+    Turkey           :{cname:'土耳其',littleimg:'Turkey.png',bigimg:'Turkey-b.png',ename:'Turkey'},
+    Welsh            :{cname:'威尔士',littleimg:'Welsh.png',bigimg:'Welsh-b.png',ename:'Welsh'},
+    Iceland          :{cname:'冰岛',littleimg:'Iceland.png',bigimg:'Iceland-b.png',ename:'Iceland'},
+    NorthernIreland :{cname:'北爱尔兰',littleimg:'NorthernIreland.png',bigimg:'NorthernIreland-b.png',ename:'NorthernIreland'},
+    Albania          :{cname:'阿尔巴尼亚',littleimg:'Albania.png',bigimg:'Albania-b.png',ename:'Albania'},
+    Czech            :{cname:'捷克',littleimg:'Czech.png',bigimg:'Czech-b.png',ename:'Czech'},
+    Ireland          :{cname:'爱尔兰',littleimg:'Ireland.png',bigimg:'Ireland-b.png',ename:'Ireland'},
+    Spain            :{cname:'西班牙',littleimg:'Spain.png',bigimg:'Spain-b.png',ename:'Spain'}
+};
+
+var CHOOSE_TEAM = ['France','England','Spain','Czech','Italy','Welsh'];
